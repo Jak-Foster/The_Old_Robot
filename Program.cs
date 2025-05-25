b66bf6c3e14ad78ca6a0b482517cdb3e9a24b0bc
JakFoster <jakfoster97@outlook.com>
2025-05-25 21:58:52 +0100

    Completed room coordinates challenge

--- Program.cs
+++ Program.cs
@@ -1,30 +1,45 @@
 ﻿using The_Old_Robot;
 using The_Old_Robot.Power;
 using The_Old_Robot.Direction;
+using The_Old_Robot.Room_Coordinates;
 
 internal class Program
 {
     private static void Main()
     {
-        Robot Robot = new();
+        //Robot Robot = new();
 
-        Console.WriteLine("Enter three commands for your robot!");
+        //Console.WriteLine("Enter three commands for your robot!");
 
-        for (int I = -1; I++ < 2; )
-        {
-            IRobotCommand UserInput = Console.ReadLine() switch
-            {
-                "On" => new OnCommand(),
-                "Off" => new OffCommand(),
-                "North" => new NorthCommand(),
-                "South" => new SouthCommand(),
-                "East" => new EastCommand(),
-                "West" => new WestCommand()
-            };
+        //for (int I = -1; I++ < 2; )
+        //{
+        //    IRobotCommand UserInput = Console.ReadLine() switch
+        //    {
+        //        "On" => new OnCommand(),
+        //        "Off" => new OffCommand(),
+        //        "North" => new NorthCommand(),
+        //        "South" => new SouthCommand(),
+        //        "East" => new EastCommand(),
+        //        "West" => new WestCommand()
+        //    };
 
-            Robot.Commands[I] = UserInput;
-        }
+        //    Robot.Commands[I] = UserInput;
+        //}
 
-        Robot.Run();
+        //Robot.Run();
+
+
+        Coordinate Room1 = new(0, 1);
+        Coordinate Room2 = new(3, 2);
+        Coordinate Room3 = new(5, 6);
+        Coordinate Room4 = new(4, 7);
+        Coordinate Room5 = new(4, 6);
+
+        Console.WriteLine(Room1.IsAdjacent(Room2));
+        Console.WriteLine(Room2.IsAdjacent(Room3));
+        Console.WriteLine(Room3.IsAdjacent(Room4));
+        Console.WriteLine(Room4.IsAdjacent(Room5));
+        Console.WriteLine(Room5.IsAdjacent(Room1));
+        Console.WriteLine(Room3.IsAdjacent(Room3));
     }
 }
---
+++ Room_Coordinates/Coordinate.cs
@@ -0,0 +1,22 @@
+﻿
+namespace The_Old_Robot.Room_Coordinates
+{
+    public readonly struct Coordinate(int Row, int Column)
+    {
+        public readonly int Row = Row;
+        public readonly int Column = Column;
+
+        public bool IsAdjacent(Coordinate OtherCoordinate)
+        {
+            if ((Row + 1 == OtherCoordinate.Row || Row - 1 == OtherCoordinate.Row) && Column == OtherCoordinate.Column)
+            {
+                return true;
+            }
+            if ((Column + 1 == OtherCoordinate.Column || Column - 1 == OtherCoordinate.Column) && Row == OtherCoordinate.Row)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
